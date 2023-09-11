f1b6434b5f90af72524cefa2167e5037eb56ff05
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2023-09-11 15:04:05 -0300

    ajustando base do projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from './components/header';
+import Post from './post';
+import './styles.css';
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Post />
+    </>
+  )
+}
+
+export default App
---
+++ src/post.jsx
@@ -0,0 +1,7 @@
+function Post() {
+    return (
+        <h1>teste</h1>
+    )
+}
+
+export default Post;
